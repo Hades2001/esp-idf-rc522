5317ac1b0b44b04794ef5979b0151d243c737c91
hades <hades2001@foxmail.com>
2024-12-23 13:09:29 +0800

    add ntag readn funtion

--- include/picc/rc522_ntag.h
+++ include/picc/rc522_ntag.h
@@ -10,8 +10,8 @@ extern "C" {
 #define NTAG_PAGE_SIZE  (4)
 #define NTAG_PAGE_READ_SIZE (16)
 
-esp_err_t rc522_ntag_read(const rc522_handle_t rc522, const rc522_picc_t *picc, uint8_t block_address,
-    uint8_t out_buffer[NTAG_PAGE_SIZE]);
+esp_err_t rc522_ntag_read(const rc522_handle_t rc522, const rc522_picc_t *picc, uint8_t block_address,uint8_t out_buffer[NTAG_PAGE_SIZE]);
+esp_err_t rc522_ntag_readn(const rc522_handle_t rc522, const rc522_picc_t *picc, uint16_t address,uint8_t* out_buffer,int len);
 
 #ifdef __cplusplus
 }
--- src/picc/rc522_ntag.c
+++ src/picc/rc522_ntag.c
@@ -6,7 +6,9 @@
 #include "rc522_picc_internal.h"
 #include "picc/rc522_ntag.h"
 
-RC522_LOG_DEFINE_BASE();
+
+
+#define TAG "NTAG"
 
 enum
 {
@@ -20,7 +22,6 @@ enum
      */
     RC522_NTAG_WRITE_CMD = 0xA0,
 };
-
 
 
 esp_err_t rc522_ntag_read(const rc522_handle_t rc522, const rc522_picc_t *picc, uint8_t page_address,
@@ -62,4 +63,74 @@ esp_err_t rc522_ntag_read(const rc522_handle_t rc522, const rc522_picc_t *picc, 
     memcpy(out_buffer, block_buffer, NTAG_PAGE_SIZE); // -2 cuz of CRC_A
 
     return ESP_OK;
+}
+
+esp_err_t rc522_ntag_readn(const rc522_handle_t rc522, const rc522_picc_t *picc, uint16_t address,uint8_t* out_buffer,int len)
+{
+    if((out_buffer == NULL) ||(len <= 0)){
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    uint8_t read_buff[NTAG_PAGE_SIZE];
+    uint16_t offset,write_offset = 0;
+    uint8_t page,cpy_num;
+    int buff_len = len;
+
+    page = address / 4;
+    offset = address % 4;
+    len += offset;
+
+    while(len > 0){
+        rc522_ntag_read(rc522,picc,page,read_buff);
+        len -= 4;
+        page++;
+        cpy_num = (len < 0 ) ? (4 - offset + len):(4-offset);
+        ESP_LOG_BUFFER_HEX_LEVEL(TAG,&read_buff[offset],4,ESP_LOG_INFO);
+        memcpy((out_buffer + write_offset),&read_buff[offset],4-offset);
+        offset = (offset != 0 ) ? 0 : offset;
+        write_offset += cpy_num;
+        ESP_LOGI(TAG,"len:%d,page:%d,cpy_num:%d,offset:%d",len,page,cpy_num,offset);
+    }
+    //ESP_LOG_BUFFER_HEX_LEVEL(TAG,out_buffer,buff_len,ESP_LOG_INFO);
+
+    return ESP_OK; 
+}
+
+
+typedef struct ntag_tvl_info{
+    uint8_t type;
+    int blocklen;
+    int start_addr;
+}ntag_tvl_info_t;
+
+esp_err_t ntag_get_tlv_info(const rc522_handle_t rc522, const rc522_picc_t *picc, ntag_tvl_info_t* tvl_info)
+{
+    if( tvl_info == NULL ){
+        return ESP_ERR_INVALID_ARG;
+    }
+
+    uint8_t tvl_buff[NTAG_PAGE_SIZE];
+    ESP_RETURN_ON_ERROR(rc522_ntag_read(rc522,picc,4,tvl_buff),TAG,"NTAG read fault");
+   
+    tvl_info->type = tvl_buff[0];
+    if(tvl_buff[1] != 0xff){
+        tvl_info->blocklen = tvl_buff[1];
+        tvl_info->start_addr = 4*4+2;
+    } else
+    {
+        tvl_info->blocklen = ((int)tvl_buff[2] << 8) + tvl_buff[3];
+        tvl_info->start_addr = 5*4;
+    }
+    return ESP_OK;
+}
+
+typedef struct ntag_ndef_info{
+    uint8_t type;
+    int blocklen;
+    int start_addr;
+}ntag_ndef_info;
+
+esp_err_t ntag_NDEF_index(const rc522_handle_t rc522, const rc522_picc_t *picc)
+{
+
 }
